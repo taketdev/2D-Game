e34d10f1a427a8876955e631f0af06ef4b799181
taketdev <maxleyh17@proton.me>
2025-10-14 14:29:43 +0200

    added movable object and world class js

---
+++ models/movable_object.class.js
@@ -0,0 +1,19 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    // loadImage('image/test.png');
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
+        this.img.src = path; 
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
