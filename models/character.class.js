b68120a6f7b18fd699e95c82ae5eeb235d3c3b10
taketdev <maxleyh17@proton.me>
2025-10-14 14:29:09 +0200

    added character and chicken class js

---
+++ models/character.class.js
@@ -0,0 +1,15 @@
+class Character extends MovableObject{
+
+
+    constructor() {
+        super().loadImage('./img_pollo_locco/img/2_character_pepe/2_walk/W-21.png')
+    }
+
+    moveRight(){
+
+    }
+
+    jump(){
+
+    }
+}
---
+++ models/chicken.class.js
@@ -0,0 +1,11 @@
+class Chicken extends MovableObject{
+
+
+    constructor() {
+        super().loadImage('./img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk')
+    }
+
+    moveLeft(){
+
+    }
+}
